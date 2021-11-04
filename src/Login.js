76df00120665c008b18216d780f41b636d95a630
Misael <927misael@gmail.com>
2021-11-04 19:34:46 -0300

    alterações em login e news

--- src/Login.js
+++ src/Login.js
@@ -7,6 +7,108 @@ function Login(){
         <div class="login">
             <div class="header">
                 <h1>Adentre no U64</h1>
+                <h2>Faça login</h2>
+        <div class="form box">
+            <form>
+                <label class="question">Email:</label>
+                <br/>
+                <br/>
+                <input type="text" name=""/>
+                <br/>
+                <br/>
+                <label class="question">Senha:</label>
+                <br/>
+                <br/>
+                <input type="password" name=""/>
+                <br/>
+                <br/>
+                <input class="button" type="button" onclick="alert('Login realizado com sucesso!')" value="Fazer login"/>
+                <br/>
+            </form>
+        </div>
+
+        <h2>Crie sua conta</h2>
+        <div class="form box">
+            <form>
+                <h4>Preencha os dados:</h4>
+                <label class="question">Email:</label>
+                <br/>
+                <br/>
+                <input type="text" name=""/>
+                <br/>
+                <br/>
+                <label class="question">Senha:</label>
+                <br/>
+                <br/>
+                <input type="password" name=""/>
+                <br/>
+                <br/>
+                <label class="question">Confirmar senha:</label>
+                <br/>
+                <br/>
+                <input type="password" name=""/>
+                <br/>
+                <br/>
+                <div class="options">
+                    <label class="question">Quão bem você joga xadrez?</label><br/><br/>
+
+                    <input type="radio" name="createacc" id="leigo" value="1"/>
+                    <label for="1">Nunca joguei</label><br/><br/>
+
+                    <input type="radio" name="createacc" id="basico" value="2"/>
+                    <label for="2">Sei mover as peças</label><br/><br/>
+
+                    <input type="radio" name="createacc" id="engajado" value="3"/>
+                    <label for="3">Jogo casualmente</label><br/><br/>
+
+                    <input type="radio" name="createacc" id="intermediario" value="4"/>
+                    <label for="4">Jogo com frequência</label><br/><br/>
+
+                    <input type="radio" name="createacc" id="avançado" value="5"/>
+                    <label for="5">Meu rating é 2000+</label><br/><br/>
+                </div>
+
+                <div class="options">
+                    <label class="question">Selecione seus jogadores favoritos</label><br/><br/>
+
+                    <input type="checkbox"/>
+                    <label>Magnus Carlsen</label><br/><br/>
+
+                    <input type="checkbox"/>
+                    <label>Fabiano Caruana</label><br/><br/>
+
+                    <input type="checkbox"/>
+                    <label>Ding Liren</label><br/><br/>
+
+                    <input type="checkbox"/>
+                    <label>Garry Kasparov</label><br/><br/>
+
+                    <input type="checkbox"/>
+                    <label>Anatoly Karpov</label><br/><br/>
+
+                    <input type="checkbox"/>
+                    <label>Tigran Petrosian</label><br/><br/>
+
+                    <input type="checkbox"/>
+                    <label>Bobby Fischer</label><br/><br/>
+
+                    <input type="checkbox"/>
+                    <label>Hikaru Nakamura</label><br/><br/>
+
+                    <input type="checkbox"/>
+                    <label>Emanuel Lasker</label><br/><br/>
+
+                    <input type="checkbox"/>
+                    <label>Paul Morphy</label><br/><br/>
+
+                    <input type="checkbox"/>
+                    <label> Danill Dubov</label><br/><br/>
+            </div>
+
+            <input class="button" type="button" onclick="alert('Conta registrada com sucesso!')" value="Registrar conta"/>
+
+            </form>
+        </div>
             </div>
         </div>
     );
--- src/News.js
+++ src/News.js
@@ -11,8 +11,27 @@ function News(){
 
             <h2>Torneios</h2>
 
-            <p class="alert">Faltam poucos meses pro campeonato mundial de 2021</p>
+            <p class="alert">Faltam poucas semanas pro campeonato mundial de 2021</p>
             
+            <div class="box">
+                <h2>Campeonato dos EUA</h2>
+                <p>Pela terceira vez em sua carreira, <b>Wesley So</b> vence o terceiro campeonato dos Estados Unidos, derrubando
+                    gigantes no xadrez como <b>Fabiano Caruana</b> e <b>Hikaru Nakamura</b>. O jogador tem tido um crescimento notório e é
+                    considerado por muitos um potencial adversário do campeão <b>Magnus Carlsen</b> no campeonato mundial de 2022. Wesley
+                    teve de enfrentar Caruana e o GM <b>Sam Servian</b> nos <i>tie-breaks</i>, conseguindo encontrar uma vitória depois de um
+                    vacilo de Caruana no fim de jogo. Sua habilidade com partidas mais rápidas é unanimamente reconhecida, e foi o que lhe trouxe o título.
+                </p>
+                <p> Wesley So confessa que sua habilidade dependia um pouco da sorte no último dia, e também
+                    reconhece a habilidade extraordinária do desafiante ao título do mundial de 2018: <i>"Eu achei que o torneio tinha acabado
+                        ontem, Fabiano ganhou quase três jogos em sequência. Me foi dado um novo sopro de 
+                    vida que o torneio não tivesse acabado ainda, que eu ainda tinha chances. Eu só queria jogar rápido 
+                    hoje e fazer uns bons lances. Fabi joga pra vencer com brancas ou negras, e logo eu me encontrei em uma
+                    posição muito difícil. Ele provavelmente perdeu algum lance vitorioso no fim do jogo em algum momento.
+                    Eu estava jogando sem pressão: Eu não esperava chegar nos playoffs, então eu só estava jogando xadrez:
+                     Se chances vêm, ótimo. Se eu perder, tá tudo bem."</i>
+                </p>
+            </div>
+
             <div class="box">
                 <h2>Campeonato mundial</h2>
                 <p> <b>Magnus Carlsen</b> defenderá seu título no final de 2021 contra o desafiante <b>Ian Nepomniachtchi</b>,
