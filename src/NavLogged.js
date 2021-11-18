0ab4b5383330051bb0a5fc81e4bf6afb1a3695f8
Misael <927misael@gmail.com>
2021-11-18 20:59:32 -0300

    pequenos ajustes

--- src/NavLogged.js
+++ src/NavLogged.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState } from 'react';
 
 import './stylesheets/style.css';
 import './stylesheets/nav.css';
 
+import { collection, getDocs} from 'firebase/firestore';
+import { useAuth } from './contexts/AuthContext';
+import {db} from './services/firebase';
+
 function NavLogged(){
 
+    const {currentUser } = useAuth();
+    const [users, setUsers] = useState([]);
+    const usersCollectionRef = collection(db, "users")
+    
+    useEffect(() =>  {
+
+        const getUsers = async () => {
+            const data = await getDocs(usersCollectionRef);
+            setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+        }
+
+        getUsers();
+    }, [])
 
     return(
 
@@ -19,7 +36,13 @@ function NavLogged(){
                     <li><a href="/about">Sobre nós</a></li>
                 </ul>
                 <ul className="nav">
-                    <li><a className="button" href="/account">Meu perfil</a></li>
+                    {users.map((user) => {
+                        if(user.email === currentUser.email){
+                            return(
+                                <li><a className="button" href="/account">{user.nick}</a></li>
+                            )
+                    }})}
+                    
                 </ul>
             </nav>
         </header>
